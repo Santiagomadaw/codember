4c58e2db9330c0533760b3076f272e1539208d2d
Santiagomadaw <santiagoma.daw@gmail.com>
2023-11-24 13:40:16 +0100

    first commit

---
+++ challenge01/challenge1.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+def open_file(text_file: str) -> str:
+    with open(text_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+def count_words(list_words): 
+    counted_words = dict()
+    for word in list_words: 
+        if word in counted_words:
+            counted_words[word] += 1
+        else:
+            counted_words[word] = 1
+    return counted_words
+
+def load_text(file:str):
+    message = open_file(file).lower()
+    counted_words = count_words(message.split())
+    hidden_message =reduce(lambda accum, word:accum + word + str(counted_words[word]), counted_words)
+    return hidden_message
+
+print(load_text('C:\\Users\\karho\\Desktop\\P101\\codember\\challenege01\\message_01.txt'))
