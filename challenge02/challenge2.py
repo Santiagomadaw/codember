fd3834b31caad8fc99a895a29a274c648dd1cd9c
Santiagomadaw <santiagoma.daw@gmail.com>
2023-11-24 18:30:14 +0100

    Segundo reto

--- challenge02/challenge2.py
+++ challenge02/challenge2.py
@@ -1,19 +1,19 @@
 
 rosseta = {'#':lambda accum,x: (accum, x + 1),
            '@':lambda accum,x: (accum, x - 1),
-           '*':lambda accum,x: (accum,x**2),
+           '*':lambda accum,x: (accum, x**2),
            '&':lambda accum,x: (accum + str(x), x)}
+
 def open_file(text_file: str) -> str:
     with open(text_file, 'r', encoding='utf-8') as file:
         content = file.read()
     return content
+
 def decoder(file:str):
-    code = '&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&'
-    start = 0
-    message = ''
+    code = open_file(file)
+    start,message = 0,''
     for char in code:
-        message, start=rosseta[char](message, start)
-        print(char, start, message)
+        message, start = rosseta[char](message, start)   
     return message
 print(decoder('message_02.txt'))
 
