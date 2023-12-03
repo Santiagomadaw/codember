ae842cdab3a7c3dc560611da0df0e83fb8881f00
Santiagomadaw <santiagoma.daw@gmail.com>
2023-12-03 09:30:43 +0100

    Clarificado codigo

--- challenge03/challenge3.py
+++ challenge03/challenge3.py
@@ -1,12 +1,24 @@
 import re
-from functools import reduce
 
-def decoder(file: str, number:int):
-    with open(file, 'r', encoding='utf-8') as file: content_list = file.read().split('\n')
+
+def is_corrupted(code):
+    return not (int(code[0]) <= code[3].count(code[2]) <= int(code[1]))
+
+
+def open_file(file):
+    with open(file, 'r', encoding='utf-8') as file:
+        content_list = file.read().split('\n')
+    return content_list
+
+
+def decoder(file: str, number: int):
+
+    content_list = open_file(file)
+
     splited_codes = map(lambda code: re.split(r'[-\s:]+', code), content_list)
-    corrupted_codes = list(filter(lambda code:    not (int(code[0]) <= code[3].count(code[2]) <= int(code[1])) , splited_codes))
-    
-    
+    corrupted_codes = filter(lambda code: is_corrupted(code), splited_codes)
+    corrupted_codes = list(corrupted_codes)
     return corrupted_codes[number-1][3]
+
 
 print(decoder('encryption_policies.txt', 42))
